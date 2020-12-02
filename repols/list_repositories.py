c8693281c7598dbe361b9f24c4af7dfccd9544e3
deriksson <gustaf.daniel.eriksson@gmail.com>
2020-12-02 23:55:01 +0100

    Use typed function signatures

--- repols/list_repositories.py
+++ repols/list_repositories.py
@@ -2,7 +2,7 @@ from getpass import getpass
 from github import Github
 
 
-def list_repositories(organisation, team):
+def list_repositories(organisation: str, team: str) -> None:
     token = getpass(prompt="Token:")
     g = Github(base_url="https://api.github.com", login_or_token=token)
     o = g.get_organization(organisation)
