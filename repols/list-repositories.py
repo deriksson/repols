3e24dd6922ebe216b465470618f228941a4006f1
deriksson <gustaf.daniel.eriksson@gmail.com>
2020-12-02 23:14:26 +0100

    Create project skeleton

---
+++ repols/list-repositories.py
@@ -0,0 +1,10 @@
+from getpass import getpass
+from github import Github
+
+
+def list_repositories(organisation, team):
+    token = getpass(prompt='Token:')
+    g = Github(base_url="https://api.github.com", login_or_token=token)
+    o = g.get_organization(organisation)
+    for repo in o.get_team_by_slug(team).get_repos():
+        print(repo.name)
---
+++ setup.py
@@ -0,0 +1,27 @@
+"""Allow easy installation of this package.
+
+"""
+
+# Third-party imports
+from setuptools import setup, find_packages
+
+
+setup(
+    name='repols',
+    version='0.0.1',
+    author='Daniel Eriksson',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click >= 7.0',
+        'PyGithub'
+    ],
+    setup_requires=[
+        'flake8'
+    ],
+    entry_points={
+        'console_scripts': [
+            'repols=repols.cli:cli'
+        ]
+    }
+)
