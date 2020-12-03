7b75af471d56e581afbdfefa9929796b17f3feb3
deriksson <gustaf.daniel.eriksson@gmail.com>
2020-12-03 23:49:11 +0100

    Add black to lint task

--- test/test_body.py
+++ test/test_body.py
@@ -2,10 +2,9 @@ import unittest
 
 
 class TestListRepositories(unittest.TestCase):
-
     def test_create_body_with_required_structure(self):
         self.assertTrue(True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
