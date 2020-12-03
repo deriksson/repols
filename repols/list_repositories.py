928e40fe8740874aae5bf531bcb4e7b32e381d59
deriksson <gustaf.daniel.eriksson@gmail.com>
2020-12-03 01:46:01 +0100

    Output a sorted list of repositories

--- repols/list_repositories.py
+++ repols/list_repositories.py
@@ -1,10 +1,17 @@
 from getpass import getpass
+from bisect import insort_left
 from github import Github
 
 
 def list_repositories(organisation: str, team: str) -> None:
     token = getpass(prompt="Token:")
+
     g = Github(base_url="https://api.github.com", login_or_token=token)
     o = g.get_organization(organisation)
+
+    repos = []
     for repo in o.get_team_by_slug(team).get_repos():
-        print(repo.name)
+        insort_left(repos, repo.name)
+
+    for repo in repos:
+        print(repo)
