911d588275acbccd8241ae39b54d61fa74eceef1
deriksson <gustaf.daniel.eriksson@gmail.com>
2020-12-03 18:37:42 +0100

    Use better variable names

--- repols/list_repositories.py
+++ repols/list_repositories.py
@@ -3,14 +3,13 @@ from bisect import insort_left
 from github import Github
 
 
-def list_repositories(organisation: str, team: str) -> None:
+def list_repositories(organisation_name: str, team: str) -> None:
     token = getpass(prompt="Token:")
-
-    g = Github(base_url="https://api.github.com", login_or_token=token)
-    o = g.get_organization(organisation)
+    organisation = Github(base_url="https://api.github.com", login_or_token=token)\
+        .get_organization(organisation_name)
 
     repos = []
-    for repo in o.get_team_by_slug(team).get_repos():
+    for repo in organisation.get_team_by_slug(team).get_repos():
         insort_left(repos, repo.name)
 
     for repo in repos:
