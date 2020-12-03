5120ae539609bfb3ca8f386a506583488ffa3f69
deriksson <gustaf.daniel.eriksson@gmail.com>
2020-12-03 23:34:56 +0100

    Use tox to run unit tests

---
+++ test/test_body.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestListRepositories(unittest.TestCase):
+
+    def test_create_body_with_required_structure(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
