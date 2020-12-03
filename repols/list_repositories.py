4107619e9b7600d112a020caed223792fd08d4bb
deriksson <gustaf.daniel.eriksson@gmail.com>
2020-12-03 18:41:00 +0100

    Use standard indentation for dot notation

--- repols/list_repositories.py
+++ repols/list_repositories.py
@@ -5,8 +5,9 @@ from github import Github
 
 def list_repositories(organisation_name: str, team: str) -> None:
     token = getpass(prompt="Token:")
-    organisation = Github(base_url="https://api.github.com", login_or_token=token)\
-        .get_organization(organisation_name)
+    organisation = Github(
+        base_url="https://api.github.com", login_or_token=token
+    ).get_organization(organisation_name)
 
     repos = []
     for repo in organisation.get_team_by_slug(team).get_repos():
