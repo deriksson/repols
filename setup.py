24635076aa9a269414cc43094c2c2a70ca7016b7
deriksson <gustaf.daniel.eriksson@gmail.com>
2020-12-21 01:38:53 +0100

    [release] Generate Markdown from Asciidoc

--- setup.py
+++ setup.py
@@ -2,13 +2,27 @@
 
 """
 
-# Third-party imports
 from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
 
 setup(
     name="repols",
-    version="0.0.1",
+    version="1.0.0",
     author="Daniel Eriksson",
+    author_email="gustaf.daniel.eriksson@gmail.com",
+    description="List GitHub repositories filtered by team",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deriksson/repols",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["Click >= 7.0", "PyGithub"],
